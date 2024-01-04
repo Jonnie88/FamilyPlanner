84d3a0b3bda89ddc32222777b36f523d6a8a6f7c
Johannes <Johanneslundberg88@gmail.com>
2024-01-04 13:32:49 +0100

    standard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
 
 function App() {
-  let data = [
-    { firstname: "Johannes", lastname: "Lundberg", birthdate: "1988-11-16" },
-    { firstname: "Charlie", lastname: "Lundberg", birthdate: "1988-11-16" },
-    { firstname: "Edith", lastname: "Lundberg", birthdate: "1988-11-16" },
-    { firstname: "Märta", lastname: "Lundberg", birthdate: "1988-11-16" },
-    { firstname: "Gösta", lastname: "Lundberg", birthdate: "1988-11-16" },
-    { firstname: "Emelie", lastname: "Johansson", birthdate: "1988-11-16" },
-  ];
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  return <>hello world</>;
 }
 
 export default App;
